f8a58ddb9490e17dfed81496361a2dea3a106aef
Jeff Cacossa <jeff.cacossa@gmail.com>
2020-10-06 14:20:09 -0400

    Trying to debug startScanning

--- node_helper.js
+++ node_helper.js
@@ -16,34 +16,57 @@ module.exports = NodeHelper.create({
         console.log('MMM-AirMentorPro registered');
         // this.tvoc = 0;
 
-        noble.startScanning();
-        // setInterval(function(){noble.stopScanning();noble.startScanning();}, 5000);
+        // noble.startScanning();
+        setInterval(function(){
+            console.log('-------AirMentorPro setInterval running');
+            noble.startScanning([], false);
+            noble.on('stateChange', function(state) {
+                console.log('-------noble state changed')
+                    if (state === 'poweredOn') {
+                      noble.startScanning([], false);
+                    }
+            });
+        
+        }, 10000);
 
-        noble.on('discover', function(peripheral) {
-            let macAddress = peripheral.uuid;
-            let rssi = peripheral.rssi;
-            let localName = peripheral.advertisement.localName;
+        // noble.on('stateChange', function(state) {
+        //     if (state === 'poweredOn') {
+        //       noble.startScanning([], true);
+        //     }
+        //   });
+        
+        // noble.on('stateChange', function(state) {
+        //     if (state === 'poweredOn')
+        //       noble.startScanning();
+        //     else
+        //       noble.stopScanning();
+        // });
+
+        // noble.on('discover', function(peripheral) {
+        //     let macAddress = peripheral.uuid;
+        //     let rssi = peripheral.rssi;
+        //     let localName = peripheral.advertisement.localName;
 
 
-            console.log('found device: ', macAddress, ' ', localName, ' ', rssi);
+        //     console.log('found device: ', macAddress, ' ', localName, ' ', rssi);
 
-            if (localName == 'Air Mentor Pro' || macAddress == target){
-                let data = peripheral.advertisement.manufacturerData.toString('hex');
-                console.log(data);
+        //     if (localName == 'Air Mentor Pro' || macAddress == target){
+        //         let data = peripheral.advertisement.manufacturerData.toString('hex');
+        //         console.log(data);
 
-                let _tvoc = parseInt(data.slice(4,8), 16);
-                let _iaq = parseInt(data.slice(16,), 16);
-                console.log('tvoc ' + tvoc);
-                // console.log('iaq ' + iaq);	
+        //         let _tvoc = parseInt(data.slice(4,8), 16);
+        //         let _iaq = parseInt(data.slice(16,), 16);
+        //         console.log('tvoc ' + tvoc);
+        //         // console.log('iaq ' + iaq);	
 
-                this.config.tvoc = _tvoc;
-                // iaq = _iaq;
+        //         this.config.tvoc = _tvoc;
+        //         // iaq = _iaq;
 
-                this.updateDom();
+        //         this.updateDom();
 
-            }
+        //     }
 
-        });
+        // });
 
         
     },
