faa222de23c289f4792a3f4d0bfbc83f3b124e44
Jeff Cacossa <jeff.cacossa@gmail.com>
2020-10-08 10:18:51 -0400

    Cleanup

--- MMM-AirMentorPro.js
+++ MMM-AirMentorPro.js
@@ -1,12 +1,3 @@
-// Based on: 
-// https://github.com/philippeportesppo/AirMentorPro2_SmartThings
-// https://blog.truthlabs.com/beacon-tracking-with-node-js-and-raspberry-pi-794afa880318
-// Correction at https://gist.github.com/eklimcz/b37c05b29d9ac7cdd040#file-gistfile1-js
-// https://github.com/noble/noble
-
-
-
-
 Module.register("MMM-AirMentorPro",{
 
 	// Default module config.
@@ -16,22 +7,17 @@ Module.register("MMM-AirMentorPro",{
         
     },
 
-    tvoc: 0,
+    aqi: 0,
 
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-        // wrapper.innerHTML = "tvoc " + this.tvoc;
-        wrapper.innerHTML = "tvoc " + this.tvoc;
-        console.log("----------AirMentorPro debugging...")
-        console.log(this.tvoc);
-        console.log(this);
+        wrapper.innerHTML = "AQI: " + this.aqi;
 		return wrapper;
     },
 
     start: function () {
 
-        // var tvoc = 1;
         var self = this;
 
         setInterval(function(){
@@ -39,24 +25,16 @@ Module.register("MMM-AirMentorPro",{
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = function() {
                 if (xhr.readyState == XMLHttpRequest.DONE) {
-                    console.log("---------Received xhr: " + xhr.responseText);
-                    // this.tvoc = xhr.responseText;
-                    self.tvoc = xhr.responseText;
-                    console.log(this);
-                    console.log(self);
+                    console.log("--------- Received xhr: " + xhr.responseText);
+                    self.aqi = xhr.responseText;
                 }
             }
             xhr.open('GET', 'http://jeff.local:7000', true);
             xhr.send(null);
             self.updateDom();
 
-
         }, 10000);
-
     },
-
-
-    
 
 });
 
