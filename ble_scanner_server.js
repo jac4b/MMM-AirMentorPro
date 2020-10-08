41dbd57372ce2668456318b5614376c7fc0e3b7f
Jeff Cacossa <jeff.cacossa@gmail.com>
2020-10-08 15:17:46 -0400

    Clean up server

--- ble_scanner_server.js
+++ ble_scanner_server.js
@@ -35,53 +35,7 @@ noble.on('discover', function(peripheral) {
     let data = peripheral.advertisement.manufacturerData.toString('hex');
     console.log(data);
 
-    let id = data.slice(0,4);
-    console.log(id);
-
-    let firstName;
-    let secondName;
-    
-    let firstData = parseInt(data.slice(4,8), 16);
-    let secondData = parseInt(data.slice(16,), 16);
-
-    let firstDesc;
-    let secondDesc;
-
-    if (id == 2221){
-      if (firstData < 312) { firstDesc = "good" } else
-      if (firstData < 560) { firstDesc = "moderate" } else
-      if (firstData < 1000) { firstDesc = "unhealthy" } else
-      if (firstData < 3000) { firstDesc = "very unhealthy" } else
-      { firstDesc = "worst" };
-
-      if (secondData < 50) { secondDesc = "good" } else
-      if (secondData < 100) { secondDesc = "moderate" } else
-      if (secondData < 150) { secondDesc = "unhealthy" } else
-      if (secondData < 200) { secondDesc = "very unhealthy" } else
-      { secondDesc = "worst" };
-
-      firstName = 'TVOC';
-      secondName = 'IAQ';
-    }
-    else {
-      if (firstData < 800) { firstDesc = "good" } else
-      if (firstData < 1000) { firstDesc = "moderate" } else
-      if (firstData < 2000) { firstDesc = "unhealthy" } else
-      if (firstData < 3000) { firstDesc = "very unhealthy" } else
-      { firstDesc = "worst" };
-
-      if (secondData < 15400) { secondDesc = "good" } else
-      if (secondData < 40400) { secondDesc = "moderate" } else
-      if (secondData < 65500) { secondDesc = "unhealthy" } else
-      if (secondData < 150500) { secondDesc = "very unhealthy" } else
-      { secondDesc = "worst" };
-
-      firstName = 'CO2';
-      secondName = 'PM25';   
-    }
-
-    // aqi = firstName + ': ' + firstData + ' ' + firstDesc + ' | ' + secondName + ': ' + secondData + ' ' + secondDesc;  
-    // console.log(aqi);   
+  
     aqi = data; 
 
   }
